0ab93b9d057e1cc852e7530fe0093b86b9d6fa7c
mdYeasinIslam <hyeasinislam@gmail.com>
2024-12-26 00:37:51 +0600

    filtering related funtionality implement

--- src/pages/BlogSection/AllBlogs/AllBlogs.tsx
+++ src/pages/BlogSection/AllBlogs/AllBlogs.tsx
@@ -1,18 +1,41 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import axios from 'axios'
 import { BlogType } from "../../../Types/types";
 import Skeleton from "react-loading-skeleton";
 import CommonHeader from "../../../Component/CommonHeader/CommonHeader";
 import DisplayBlog from "../../Home/LatestBlogs/DisplayBlog";
+import { Box, Divider, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
 
 const AllBLogs = () => {
     const [latestBlogs, setLatestBlogs] = useState<BlogType[]>([])
+    const [searchText, setSearchText] = useState('')
+    const [category, setCategory] = useState('')
+    const [noFound, setNoFound] = useState('')
+
     useEffect(() => {
-        fetchData()
-    }, [])
-    const fetchData = async () => {
-        const response = await axios.get(`${import.meta.env.VITE_server}/allBlog?allBlog=all-blog`)
+        fetchData(searchText, category)
+    }, [searchText, category])
+    const fetchData = async (text: string, category: string) => {
+        const response = await axios.get(`${import.meta.env.VITE_server}/allBlog?allBlog=all-blog&search=${text}`)
+        if (response?.data?.status === false) {
+            return setNoFound(response?.data?.message)
+        }
+        if (category.length > 0) {
+            const filter = response?.data?.filter((d: BlogType) => d?.categories?.includes(category))
+            setNoFound('')
+            return setLatestBlogs(filter)
+        }
+        console.log(response)
+        setNoFound('')
         setLatestBlogs(response.data)
+    }
+    console.log(noFound)
+    const searchByTitle = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value)
+        setSearchText(event.target.value)
+    }
+    const handleChange = (event: SelectChangeEvent) => {
+        setCategory(event.target.value as string);
     }
     const header = <div className="text-center dark:text-white space-y-3">
         <h1 className="text-xl md:text-4xl font-medium">Explore Recent Blogs of this week</h1>
@@ -24,8 +47,54 @@ const AllBLogs = () => {
 
                 <CommonHeader header={header} />
             </div>
+            <div className="flex justify-between items-center ">
+                <Box>
+                    <h1 className="text-2xl font-medium">Filter :</h1>
+                </Box>
+                <form className="flex gap-4">
+
+                    <FormControl>
+                        <TextField
+                            id="filled-search"
+                            label="Search field"
+                            type="search"
+                            className="border py-4 px-2"
+                            placeholder="Search by title"
+                            onChange={searchByTitle}
+                        />
+                    </FormControl>
+                    <FormControl sx={{ minWidth: 120 }} size="medium">
+                        <InputLabel id="demo-select-small-label" className="dark:text-white">Categories</InputLabel>
+                        <Select
+                            required
+                            labelId="demo-select-small-label"
+                            id="demo-select-small"
+                            value={category}
+                            label="Category"
+                            name='categories'
+                            className="dark:text-white"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value='Programming'>Programming</MenuItem>
+                            <MenuItem value='Frontend'>Frontend</MenuItem>
+                            <MenuItem value='Web Design'>Web design</MenuItem>
+                            <MenuItem value='Backend'>Backend </MenuItem>
+                            <MenuItem value='Web Development'>Web Development</MenuItem>
+                            <MenuItem value='Cloud Computing'>Cloud Computing </MenuItem>
+                            <MenuItem value='AI'>AI </MenuItem>
+                        </Select>
+                    </FormControl>
+                </form>
+
+            </div>
+            <Divider></Divider>
             {
-                !latestBlogs.length ?
+                !noFound.length ?
+                    <>
+                        {!latestBlogs.length ?
                     <div className="mt-4">
                         <Skeleton width="60%" height={20} />
                         <Skeleton width="80%" height={16} />
@@ -43,6 +112,11 @@ const AllBLogs = () => {
                             />)}
                         </div>
                        
+                            </div>}
+                    </>
+                    :
+                    <div className="font-semibold text-2xl text-center">
+                        {noFound}
                     </div>
             }
 
